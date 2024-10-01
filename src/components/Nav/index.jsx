d18aa163cc1a07ad7559622d0a5f3046ea79f362
workSilas <worksilas102@gmail.com>
2024-10-01 16:38:29 -0300

    Responsividade

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
   return (
     <div className="Nav">
-        <div className='sessao'>
-          <Link to="/">SOBRE-NÓS</Link>
-          <div className='line'></div>
-          <Link to="/">FERRAMENTAS</Link>
-          <div className='line'></div>
-          <Link to="/">CRIAR</Link>
-          <div className='line'></div>
-          <Link to="/">CONTATO</Link>
-          <div className='line'></div>
-          <Link to="/">FAQ</Link>
-          <div className='line'></div>
-          <Link to="/">ENTRAR</Link>
-        </div>
+      <div className='sessao'>
+        <Link to="/">SOBRE-NÓS</Link>
+        <div className='line'></div>
+        <Link to="/">FERRAMENTAS</Link>
+        <div className='line'></div>
+        <Link to="/">CRIAR</Link>
+        <div className='line'></div>
+        <Link to="/">CONTATO</Link>
+        <div className='line'></div>
+        <Link to="/">FAQ</Link>
+        <div className='line'></div>
+        <Link to="/">ENTRAR</Link>
+      </div>
     </div>
   );
 }
