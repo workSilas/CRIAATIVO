d3e6d99724ee1c455bd375fc5104e960471abcf1
Imsoboredd <guilhermebarreto2007@gmail.com>
2024-10-29 19:16:35 -0300

    alteração nas pag. home e ferramentas

--- src/Pages/Ferramentas/index.jsx
+++ src/Pages/Ferramentas/index.jsx
@@ -36,15 +36,15 @@ export default function Ferramentas() {
           <p>Também cuidamos do Back-end do seu site, tanto na modelação de dados, usando a linguagem <b style={{ color: "#2F00FF" }}>SQL</b>, como na criação e desenvolvimento de aplicações web (<b style={{ color: "#02B507" }}>API</b>).
             Desenvolvemos <b style={{ color: "#02B507" }}>API</b> em <b style={{ color: "#0C5602" }}>NodeJs</b>, um dos softwares de melhor desempenho quando se trata de criação de <b style={{ color: "#02B507" }}>API</b> e aplicaçôes webs.</p>
           <img src="/assets/images/ferramentasApi.png" alt="softwares" />
-          <p>Trabalhamos com banco de dados <b style={{ color: "#2F00FF" }}>MySQL</b>, que é um dos mais utilizados no mercado da tecnologia pelas grandes empresas,  criamos a estrutura e base de dados, e deixamos pronta para que possa ser integrada a sua página web.</p>
+          <p>Trabalhamos com banco de dados <b style={{ color: "#2F00FF" }}>MySQL</b>, que é um dos mais utilizados no mercado da tecnologia pelas grandes empresas,  criamos a estrutura e base de dados, e deixamos pronta para a sua página web.</p>
         </div>
       </div>
 
       <div className="sessaoTexto">
         <div className="conteudo">
-          <h2>Projetos do Figma</h2>
-          <p>Transformamos o seu desing do Figma em uma página web, bastar nos compartilhar seu projeto Figma seguindo o seguinte passo:
-            1: Com seu projeto Figma aberto, clique em <b style={{ color: "#fff", fontWeight: 12, backgroundColor: "#07A1DE", paddingLeft: 3, paddingRight: 3, borderRadius: 5 }}>Share</b> no canto superior direito.
+          <h2>Projetos do <b style={{color: "#FC1114"}}>F</b><b style={{color: "#F5595B"}}>i</b><b style={{color: "#AE35F2"}}>g</b><b style={{color: "#37BE45"}}>m</b><b style={{color: "#07A1DE"}}>a</b></h2>
+          <p>Transformamos o seu desing do Figma em uma página web, bastar nos compartilhar seu projeto Figma seguindo o seguinte passo:<br/>
+            1: Com seu projeto Figma aberto, clique em <b style={{ color: "#fff", fontWeight: 12, backgroundColor: "#07A1DE", paddingLeft: 3, paddingRight: 3, borderRadius: 5 }}>Share</b> no canto superior direito.<br/>
             2: Mande um convite para o email criaativonovoprojeto@gmail.com.
             Pronto, agora o seu projeto no Figma está sendo compartilhado conosco! E assim que escolher qual plano de desenvolvimento deseja, o prazo de entrega será informado.</p>
           <img src="/assets/images/ferramentasFigma.png" alt="softwares" />
